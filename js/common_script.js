f45e4d69f5c1bb25e074ce12609f5046aad00214
Chinmay Patil <chinmay412@gmail.com>
2014-11-24 13:18:03 +0530

    added active link functionality. working on some issues with it

---
+++ js/common_script.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+	$(".header ul li > a").click(function(){
+
+		// initialize properties to default
+		$(".header ul li > a").css('color','#666');
+		$(".header ul li").css('background','url("") no-repeat');
+
+		// set active properties
+		$(this).parent().css('background','url("images/activeNavArrow.png") no-repeat');
+		$(this).parent().css('background-position','center bottom');
+		$(this).css('color','#c00');
+
+	});
+
+	$("#cboxOverlay").click(function(){
+		
+		// set properties to default
+		$(".header ul li").css('background','url("") no-repeat');
+		$(".header ul li > a").css('color','#666');
+
+	});
+
+	// currently not working as expected
+	if($("#cboxOverlay").css("display") == "none"){
+
+		$(".header ul li").css('background','url("") no-repeat');
+		
+	}
+
+});

82c87315327ec56df8e1b700ceca821229c344fd
Chinmay Patil <chinmay412@gmail.com>
2014-12-28 09:52:29 +0530

    added seperate homepage pics. added homepage new hover effects

--- js/common_script.js
+++ js/common_script.js
@@ -1,5 +1,19 @@
 $(document).ready(function(){
 
+	var row;
+	 for (var row = 1; row <= 6; row++) {
+	 	
+	 	for (var i = 1; i <= 7; i++) {
+	 		
+	 		var objectString = ".homeGallery .row"+row+" .img"+i+" img";
+	 		var imageUrl = "images/homepage/row"+row+"_"+i+".jpg";
+
+	 		$(objectString).attr("src",imageUrl);
+
+	 	};
+
+	 };
+
 	$(".header ul li > a").click(function(){
 
 		// initialize properties to default

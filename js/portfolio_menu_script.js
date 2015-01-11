a6c8c8f1c3c994cb6ca2e747a848e414bc836cc6
Chinmay Patil <chinmay412@gmail.com>
2015-01-11 17:45:21 +0530

    re-enabled colorbox top-nav

--- js/portfolio_menu_script.js
+++ js/portfolio_menu_script.js
@@ -2,51 +2,65 @@ $(document).ready(function(){
 
 	$(".cboxElement").click(function(){
 
-		// $(".portfolioList").remove();
- 		// $("#cboxContent").append('<ul class="portfolioList clearfix animated fadeInUp de6ms"><li id="pf1"><a href="javascript:void(0);">Architectural</a><span></span></li><li id="pf2"><a href="javascript:void(0);">Industrial Product</a><span></span></li><li id="pf3"><a href="javascript:void(0);">Food Product</a><span></span></li><li id="pf4"><a href="javascript:void(0);">Jewellery</a><span></span></li><li id="pf5"><a href="javascript:void(0);">Product</a><span></span></li><li id="pf6"><a href="javascript:void(0);">People</a><span></span></li><li id="pf7"><a href="javascript:void(0);">Nature &amp; Landscape</a></li></ul>');
+		 $(".portfolioList").remove();
+ 		 $("#cboxContent").append('<ul class="portfolioList clearfix animated fadeInUp de6ms"><li id="pf1"><a href="javascript:void(0);">Architectural</a><span></span></li><li id="pf2"><a href="javascript:void(0);">Industrial Product</a><span></span></li><li id="pf3"><a href="javascript:void(0);">Food Product</a><span></span></li><li id="pf4"><a href="javascript:void(0);">Jewellery</a><span></span></li><li id="pf5"><a href="javascript:void(0);">Product</a><span></span></li><li id="pf6"><a href="javascript:void(0);">People</a><span></span></li><li id="pf7"><a href="javascript:void(0);">Nature &amp; Landscape</a></li></ul>');
 
  		$("#pf1").click(function(){
-			$("a.gallery1").colorbox({open:true});
+			$("a.home_gallery1").colorbox({open:true});
+			$(".portfolioList > li").removeClass("activeGal");
+			$(this).addClass("activeGal");
 		});
 		$("#pf2").click(function(){
-			$("a.gallery2").colorbox({open:true});
+			$("a.home_gallery2").colorbox({open:true});
+			$(".portfolioList > li").removeClass("activeGal");
+			$(this).addClass("activeGal");
 		});
 		$("#pf3").click(function(){
-			$("a.gallery3").colorbox({open:true});
+			$("a.home_gallery3").colorbox({open:true});
+			$(".portfolioList > li").removeClass("activeGal");
+			$(this).addClass("activeGal");
 		});
 		$("#pf4").click(function(){
-			$("a.gallery4").colorbox({open:true});
+			$("a.home_gallery4").colorbox({open:true});
+			$(".portfolioList > li").removeClass("activeGal");
+			$(this).addClass("activeGal");
 		});
 		$("#pf5").click(function(){
-			$("a.gallery5").colorbox({open:true});
+			$("a.home_gallery5").colorbox({open:true});
+			$(".portfolioList > li").removeClass("activeGal");
+			$(this).addClass("activeGal");
 		});
 		$("#pf6").click(function(){
-			$("a.gallery6").colorbox({open:true});
+			$("a.home_gallery6").colorbox({open:true});
+			$(".portfolioList > li").removeClass("activeGal");
+			$(this).addClass("activeGal");
 		});
 		$("#pf7").click(function(){
-			$("a.gallery7").colorbox({open:true});
+			$("a.home_gallery7").colorbox({open:true});
+			$(".portfolioList > li").removeClass("activeGal");
+			$(this).addClass("activeGal");
 		});
 
  		var galNum;
- 		if(	$(this).hasClass("gallery1") ){
+ 		if(	$(this).hasClass("home_gallery1") ){
  			galNum = 1;
  		}
- 		if(	$(this).hasClass("gallery2") ){
+ 		if(	$(this).hasClass("home_gallery2") ){
  			galNum = 2;
  		}
- 		if(	$(this).hasClass("gallery3") ){
+ 		if(	$(this).hasClass("home_gallery3") ){
  			galNum = 3;
  		}
- 		if(	$(this).hasClass("gallery4") ){
+ 		if(	$(this).hasClass("home_gallery4") ){
  			galNum = 4;
  		}
- 		if(	$(this).hasClass("gallery5") ){
+ 		if(	$(this).hasClass("home_gallery5") ){
  			galNum = 5;
  		}
- 		if(	$(this).hasClass("gallery6") ){
+ 		if(	$(this).hasClass("home_gallery6") ){
  			galNum = 6;
  		}
- 		if(	$(this).hasClass("gallery7") ){
+ 		if(	$(this).hasClass("home_gallery7") ){
  			galNum = 7;
  		}
 

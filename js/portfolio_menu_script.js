f10745ca5762bc358408f21a7cd5a491cef049a6
Chinmay Patil <chinmay412@gmail.com>
2015-01-11 10:09:45 +0530

    added missing images on homepage. fixed image miss-match linking for homepage and portfolio page

--- js/portfolio_menu_script.js
+++ js/portfolio_menu_script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 	$(".cboxElement").click(function(){
 
-		$(".portfolioList").remove();
- 		$("#cboxContent").append('<ul class="portfolioList clearfix animated fadeInUp de6ms"><li id="pf1"><a href="javascript:void(0);">Architectural</a><span></span></li><li id="pf2"><a href="javascript:void(0);">Industrial Product</a><span></span></li><li id="pf3"><a href="javascript:void(0);">Food Product</a><span></span></li><li id="pf4"><a href="javascript:void(0);">Jewellery</a><span></span></li><li id="pf5"><a href="javascript:void(0);">Product</a><span></span></li><li id="pf6"><a href="javascript:void(0);">People</a><span></span></li><li id="pf7"><a href="javascript:void(0);">Nature &amp; Landscape</a></li></ul>');
+		// $(".portfolioList").remove();
+ 		// $("#cboxContent").append('<ul class="portfolioList clearfix animated fadeInUp de6ms"><li id="pf1"><a href="javascript:void(0);">Architectural</a><span></span></li><li id="pf2"><a href="javascript:void(0);">Industrial Product</a><span></span></li><li id="pf3"><a href="javascript:void(0);">Food Product</a><span></span></li><li id="pf4"><a href="javascript:void(0);">Jewellery</a><span></span></li><li id="pf5"><a href="javascript:void(0);">Product</a><span></span></li><li id="pf6"><a href="javascript:void(0);">People</a><span></span></li><li id="pf7"><a href="javascript:void(0);">Nature &amp; Landscape</a></li></ul>');
 
  		$("#pf1").click(function(){
 			$("a.gallery1").colorbox({open:true});
